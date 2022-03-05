f27f925eee4802abdf4bd176f8d42112df002ec3
shreeyaptl <85817711+shreeyaptl@users.noreply.github.com>
2022-03-05 21:45:29 +0530

    Add files via upload

---
+++ all_new.sh
@@ -0,0 +1,38 @@
+all_new.sh  all.sh  command1.sh  command2.sh  command3.sh
+
+echo "total number of arguments are : "$#
+echo "arguments are : "$*
+echo "argument is : "$3
+
+if test -e $1
+then
+echo $1 "is exist"
+fi
+
+echo "total number of arguments are : "$#
+echo "arguments are : "$*
+echo "argument is : "$1
+
+if test -e $1
+then
+echo $1 "is exist"
+fi
+
+echo "total number of arguments are : "$#
+echo "arguments are : "$*
+echo "argument is : "$3
+
+if test -e $1
+then
+echo $1 "is exist"
+fi
+
+echo "total number of arguments are : "$#
+echo "arguments are : "$*
+echo "argument is : "$1
+
+if test -e $1
+then
+echo $1 "is exist"
+fi
+
