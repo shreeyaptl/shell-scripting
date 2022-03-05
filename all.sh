3cc1441e74bdfb03eed0eb5440f4fdcba49020d0
shreeyaptl <85817711+shreeyaptl@users.noreply.github.com>
2022-03-05 21:45:15 +0530

    Add files via upload

---
+++ all.sh
@@ -0,0 +1,14 @@
+echo "total number of arguments are : "$#
+echo "arguments are : "$*
+echo "argument is : "$3
+
+
+echo "total number of arguments are : "$#
+echo "arguments are : "$*
+echo "argument is : "$1
+
+if test -f $0
+then
+echo $0 "is exist"
+fi
+
